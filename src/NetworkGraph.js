fcb51fea30f071cd9ebccbec39e22a7b0c13087c
Christian Dalager <christian@dalager.com>
2015-02-08 21:52:44 +0100

    jshint adjustments

--- src/NetworkGraph.js
+++ src/NetworkGraph.js
@@ -1,52 +1,60 @@
-var NetworkGraph = (function () {
+var NetworkGraph = (function() {
+    "use strict";
     var timerhandle;
-    var showInfo = function (relation) {
-        console.log(relation);
+    var log = function(msg) {
+            if (enableLog && window.console) {
+                window.console.log(msg);
+            }
+        },
+        enableLog = true;
+
+    var showInfo = function(relation) {
+        log(relation);
         $('text:contains("' + relation.source.name + '")').css('font-weight', 'bold');
         $('text:contains("' + relation.target.name + '")').css('font-weight', 'bold');
         var link = $(this);
-        console.log(link);
-        $('.infobox').css('top', link.position().top+40);
-        $('.infobox').css('left', link.position().left+40);
-        $('.infobox').fadeIn();
-        $('.infobox .header').text(relation.source.name);
-        
-        $('.infobox .info').text((relation.active===0?"Former ":"")+  relation.role + ' in ' + relation.target.name);
+        log(link);
+        $(".infobox").css("top", link.position().top + 40);
+        $(".infobox").css("left", link.position().left + 40);
+        $(".infobox").fadeIn();
+        $(".infobox .header").text(relation.source.name);
+
+        $(".infobox .info").text((relation.active === 0 ? "Former " : "") + relation.role + " in " + relation.target.name);
     };
 
-    var linkMouseOut = function (relation) {
-        $('text:contains("' + relation.source.name + '")').css('font-weight', 'normal');
-        $('text:contains("' + relation.target.name + '")').css('font-weight', 'normal');
+    var linkMouseOut = function(relation) {
+        $("text:contains('" + relation.source.name + "')").css("font-weight", "normal");
+        $("text:contains('" + relation.target.name + "')").css("font-weight", "normal");
 
         clearTimeout(timerhandle);
-        timerhandle=setTimeout(function () {
-            $('.infobox').fadeOut();
+        timerhandle = setTimeout(function() {
+            $(".infobox").fadeOut();
 
         }, 12000);
-    }
+    };
 
 
-    var start = function (_width, _height, _spreadsheetId) {
-        $('.infobox').mouseover(function () {
-            console.log('mouseover infobox');
-            $('.infobox').css('top', $('.infobox').position().top + 50);
+    var start = function(_width, _height, _spreadsheetId) {
+        $(".infobox").mouseover(function() {
+            log("mouseover infobox");
+            $(".infobox").css("top", $(".infobox").position().top + 50);
         });
-        DataLoader.load(_spreadsheetId, function (links) {
+        DataLoader.load(_spreadsheetId, function(links) {
             var nodes = {};
             if (links.toArray)
                 links = links.toArray();
-            console.log(links);
+            log(links);
             // Compute the distinct nodes from the links.
-            links.forEach(function (link) {
-                link.source=link.source.trim();
-                link.target=link.target.trim();
+            links.forEach(function(link) {
+                link.source = link.source.trim();
+                link.target = link.target.trim();
                 link.source = nodes[link.source] || (nodes[link.source] = {
                     name: link.source,
-                    nodetype: 'person'
+                    nodetype: "person"
                 });
                 link.target = nodes[link.target] || (nodes[link.target] = {
                     name: link.target,
-                    nodetype: 'org',
+                    nodetype: "org",
                     orgtype: link.orgtype
                 });
             });
@@ -60,8 +68,8 @@ var NetworkGraph = (function () {
                 .links(links)
                 .size([width, height])
                 .linkDistance(90)
-                .charge(function (n) { 
-                    return n.nodetype === 'person' ? -300 : -300;
+                .charge(function(n) {
+                    return n.nodetype === "person" ? -300 : -300;
                 })
                 .linkStrength(1)
                 .on("tick", tick)
@@ -73,21 +81,30 @@ var NetworkGraph = (function () {
             var link = svg.selectAll(".link")
                 .data(force.links())
                 .enter().append("line")
-                .attr("class", function (d) {
-                    console.log(d);
-                    return d.active === "1" ? "link active" : "link inactive"
+                .attr("class", function(d) {
+                    log(d);
+                    return d.active === "1" ? "link active" : "link inactive";
                 })
-                .on('mouseover', showInfo)
-                .on('mouseout', linkMouseOut);
+                .on("mouseover", showInfo)
+                .on("mouseout", linkMouseOut);
 
+            function dragstart(d) {
+                d3.select(this).classed("fixed", d.fixed = true);
+            }
+
+            function dblclick(d) {
+                d3.select(this).classed("fixed", d.fixed = false);
+            }
+            //            var drag = force.drag().on("dragstart", dragstart);
 
             var node = svg.selectAll(".node")
                 .data(force.nodes())
                 .enter().append("g")
-                .attr("class", function (d) {
-                    console.log(d);
-                    return "node " + d.nodetype + " "+ d.orgtype;
+                .attr("class", function(d) {
+                    log(d);
+                    return "node " + d.nodetype + " " + d.orgtype;
                 })
+                // .on("dblclick", dblclick)
                 .call(force.drag);
 
 
@@ -99,27 +116,29 @@ var NetworkGraph = (function () {
             node.append("text")
                 .attr("x", 12)
                 .attr("dy", ".35em")
-                .text(function (d) {
+                .text(function(d) {
                     return d.name;
-                }).on("mouseover", function () { console.log(this) });
+                }).on("mouseover", function() {
+                    log(this)
+                });
 
             function tick() {
                 link
-                    .attr("x1", function (d) {
+                    .attr("x1", function(d) {
                         return d.source.x;
                     })
-                    .attr("y1", function (d) {
+                    .attr("y1", function(d) {
                         return d.source.y;
                     })
-                    .attr("x2", function (d) {
+                    .attr("x2", function(d) {
                         return d.target.x;
                     })
-                    .attr("y2", function (d) {
+                    .attr("y2", function(d) {
                         return d.target.y;
                     });
 
                 node
-                    .attr("transform", function (d) {
+                    .attr("transform", function(d) {
                         return "translate(" + d.x + "," + d.y + ")";
                     });
             }
